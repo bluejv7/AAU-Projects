a7cdc06b4d6356e0b9cc68f1c413fbc83e6c96d1
bluejv7 <bluejv7+github@gmail.com>
2016-07-09 03:04:12 -0700

    Update scene with new camera structure; use input axis names to control character movement; add strafing; add mouse controls

--- GAM 215/Survival Game - Part 2/Assets/Scripts/CharacterRigidBodyController.cs
+++ GAM 215/Survival Game - Part 2/Assets/Scripts/CharacterRigidBodyController.cs
@@ -7,6 +7,9 @@ using System.Collections;
 /// </summary>
 public class CharacterRigidBodyController : MonoBehaviour
 {
+    // Setup headers
+    [Header("Input Axis Names")]
+
     // serialized private variables
 
     /// <summary>
@@ -24,12 +27,100 @@ public class CharacterRigidBodyController : MonoBehaviour
     /// </summary>
     [SerializeField] private Text endingText;
 
+    /// <summary>
+    /// Object used for vertical tilting of the camera
+    /// </summary>
+    [SerializeField] private Transform cameraTarget = null;
+
+    /// <summary>
+    /// Minimum tilt value for the x-rotation of the camera
+    /// </summary>
+    [SerializeField] private float minTilt = -50.0f;
+
+    /// <summary>
+    /// Maximum tilt value fo the x-rotation of the camera
+    /// </summary>
+    [SerializeField] private float maxTilt = 30.0f;
+
+    /// <summary>
+    /// How sensitive the camera is when looking left/right
+    /// </summary>
+    [SerializeField] private float cameraSensitivityX = 2.5f;
+
+    /// <summary>
+    /// How sensitive the camera is when looking up/down
+    /// </summary>
+    [SerializeField] private float cameraSensitivityY = 2.0f;
+
+    // Movement properties
+
+    /// <summary>
+    /// The velocity we will use when moving forward
+    /// </summary>
+    [SerializeField] private float forwardVelocity = 4.0f;
+
+    /// <summary>
+    /// The velocity we will use when moving forward in the air
+    /// </summary>
+    [SerializeField] private float forwardAerialVelocity = 2.5f;
+
+    /// <summary>
+    /// The velocity we will use when moving backwards
+    /// </summary>
+    [SerializeField] private float backwardVelocity = 4.0f;
+
+    /// <summary>
+    /// The velocity we will use when moving backwards in the air
+    /// </summary>
+    [SerializeField] private float backwardAerialVelocity = 2.5f;
+
+    /// <summary>
+    /// The velocity we will use when strafing left
+    /// </summary>
+    [SerializeField] private float leftStrafeVelocity = 4.0f;
+
+    /// <summary>
+    /// The velocity we will use when strafing left in the air
+    /// </summary>
+    [SerializeField] private float leftStrafeAerialVelocity = 2.5f;
+
+    /// <summary>
+    /// The velocity we will use when strafing right
+    /// </summary>
+    [SerializeField] private float rightStrafeVelocity = 4.0f;
+
+    /// <summary>
+    /// The velocity we will use when strafing right in the air
+    /// </summary>
+    [SerializeField] private float rightStrafeAerialVelocity = 2.5f;
+
+    /// <summary>
+    /// The jump velocity
+    /// </summary>
+    [SerializeField] private float jumpVelocity = 4.0f;
+
+    /// <summary>
+    /// Input axis name for forward/backward input
+    /// </summary>
+    [SerializeField] private string moveForwardBackwardInput = "Vertical";
+
+    /// <summary>
+    /// Input axis name for left/right strafe input
+    /// </summary>
+    [SerializeField] private string moveLeftRightInput = "Horizontal";
+
+    /// <summary>
+    /// Input axis name for jump input
+    /// </summary>
+    [SerializeField] private string jumpInput = "Jump";
+
     // private variables
 
     /// <summary>
     /// The rigidbody attached to our character
     /// </summary>
     private Rigidbody rb = null;
+
     /// <summary>
     /// The capsule collider attached to our character
     /// </summary>
@@ -70,42 +161,10 @@ public class CharacterRigidBodyController : MonoBehaviour
     /// </summary>
     private Vector3 _colliderBottomOffset = Vector3.zero;
 
-    // Movement properties
-
     /// <summary>
-    /// The velocity we will use when moving forward
+    /// Current x-rotation of the camera
     /// </summary>
-    private float forwardVelocity = 4.0f;
-
-    /// <summary>
-    /// The velocity we will use when moving forward in the air
-    /// </summary>
-    private float forwardAerialVelocity = 2.5f;
-
-    /// <summary>
-    /// The velocity we will use when moving backwards
-    /// </summary>
-    private float backwardVelocity = -4.0f;
-
-    /// <summary>
-    /// The velocity we will use when moving backwards in the air
-    /// </summary>
-    private float backwardAerialVelocity = -2.5f;
-
-    /// <summary>
-    /// The amount of degrees to turn left
-    /// </summary>
-    private float leftTurnDegrees = -180.0f;
-
-    /// <summary>
-    /// The amount of degrees to turn right
-    /// </summary>
-    private float rightTurnDegrees = 180.0f;
-
-    /// <summary>
-    /// The jump velocity
-    /// </summary>
-    private float jumpVelocity = 4.0f;
+    private float currentTilt = 0;
 
     // Public methods
 
@@ -135,6 +194,54 @@ public class CharacterRigidBodyController : MonoBehaviour
     // Private methods
 
     /// <summary>
+    /// Handle horizontal movements
+    /// </summary>
+    /// <param name="forwardBackward">1 for forward and -1 for backward</param>
+    /// <param name="leftRight">1 for right and -1 for left</param>
+    private void MoveHorizontally(float forwardBackward, float leftRight)
+    {
+        // Move forward
+        if (forwardBackward > 0)
+        {
+            velocity.z = forwardBackward * (isGrounded ? forwardVelocity : forwardAerialVelocity);
+        }
+        // Move backward
+        else if (forwardBackward < 0)
+        {
+            velocity.z = forwardBackward * (isGrounded ? backwardVelocity : backwardAerialVelocity);
+        }
+
+        // Strafe left (rotate left)
+        if (leftRight < 0)
+        {
+            velocity.x = leftRight * (isGrounded ? leftStrafeVelocity : leftStrafeAerialVelocity);
+        }
+        // Strafe right (rotate right)
+        else if (leftRight > 0)
+        {
+            velocity.x = leftRight * (isGrounded ? rightStrafeVelocity : rightStrafeAerialVelocity);
+        }
+    }
+
+    /// <summary>
+    /// Make the player jump
+    /// </summary>
+    private void Jump()
+    {
+        velocity.y = jumpVelocity;
+    }
+
+    /// <summary>
+    /// Handle rotation of the camera
+    /// </summary>
+    private void RotateCamera()
+    {
+        this.transform.Rotate(0, Input.GetAxis("Mouse X") * cameraSensitivityX, 0);
+        currentTilt = Mathf.Clamp(currentTilt - Input.GetAxis("Mouse Y") * cameraSensitivityY, minTilt, maxTilt);
+        cameraTarget.localEulerAngles = new Vector3(currentTilt, 0, 0);
+    }
+
+    /// <summary>
     /// Freeze rotation on our Rigidbody so our character doesn't fall over
     /// and initialize some variables
     /// </summary>
@@ -160,7 +267,7 @@ public class CharacterRigidBodyController : MonoBehaviour
 
         // Get the bottom of the player's capsule collider by getting:
         // (center of the player) + (any offset from the center) + ((0, -1, 0) * ((half capsule height) - (radius of spherical end)))
-        // we subtract the radius of the spherical end because we're going to use a sphere with the same radius to detect collisions (is that right?)
+        // we subtract the radius of the spherical end because we're going to use a sphere with the same radius to detect collisions
         colliderBottom = this.transform.position + _colliderBottomOffset;
 
         // Simulates a sphere right below the player and returns true if it touches anything, such as the ground
@@ -170,40 +277,25 @@ public class CharacterRigidBodyController : MonoBehaviour
         if (isGrounded)
         {
             // Jump
-            if (Input.GetKey(KeyCode.Space))
+            if (Input.GetButton(jumpInput))
             {
-                velocity.y = jumpVelocity;
+                Jump();
             }
         }
 
-        // Move forward
-        if (Input.GetKey(KeyCode.W))
-        {
-            velocity.z = (isGrounded ? forwardVelocity : forwardAerialVelocity);
-        }
-        // Move backward
-        else if (Input.GetKey(KeyCode.S))
-        {
-            velocity.z = (isGrounded ? backwardVelocity : backwardAerialVelocity);
-        }
-
-        // Look left (rotate left)
-        if (Input.GetKey(KeyCode.A))
-        {
-            this.transform.Rotate(0, leftTurnDegrees * Time.deltaTime, 0);
-        }
-        // Look right (rotate right)
-        else if (Input.GetKey(KeyCode.D))
-        {
-            this.transform.Rotate(0, rightTurnDegrees * Time.deltaTime, 0);
-        }
-
+        // Handle player's horizontal movement
+        float forwardBackInput = Input.GetAxis(moveForwardBackwardInput);
+        float leftRightInput = Input.GetAxis(moveLeftRightInput);
+        MoveHorizontally(forwardBackInput, leftRightInput);
 
         // Adjust player's velocity to be an offset based on the player's rotation
         localVelocity = this.transform.TransformDirection(velocity);
 
         // Apply velocity to `rb`
         rb.velocity = localVelocity;
+
+        // Handle player's camera rotation
+        RotateCamera();
     }
 
     /// <summary>

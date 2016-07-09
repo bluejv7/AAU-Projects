e67bcca6bdaf383bab9cf34abef33e643a826c68
bluejv7 <bluejv7+github@gmail.com>
2016-07-09 01:46:42 -0700

    Add DroneController.cs and implement more to the scene

---
+++ GAM 215/Survival Game - Part 2/Assets/Scripts/DroneController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Specifies movement event triggers for the drone
+/// </summary>
+public class DroneController : MonoBehaviour {
+    /// <summary>
+    /// The damage the drone will try to impart upon the player
+    /// </summary>
+    [SerializeField] private int damage = 10;
+
+    /// <summary>
+    /// The speed at which the drone will move
+    /// </summary>
+    [SerializeField] private float speed = 4.0f;
+
+    /// <summary>
+    /// The angle at which the drone patrols around
+    /// </summary>
+    [SerializeField] private float angle = -1.0f;
+	
+	/// <summary>
+    /// Update the movement of the drone
+    /// </summary>
+	private void Update ()
+    {
+        // We're considering "right" as forward, since the model I created is facing the wrong way... =\
+        this.transform.Translate(this.transform.right * Time.deltaTime * speed);
+        this.transform.Rotate(0, angle, 0);
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.gameObject.GetComponent<CharacterRigidBodyController>().Damage(damage);
+        }
+    }
+}

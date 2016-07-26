ccf0ad8d3c5d1b06effff5577b55abc283e8f07b
bluejv7 <bluejv7+github@gmail.com>
2016-07-25 22:12:47 -0700

    Start of Marble Dungeon!

---
+++ GAM 215/Marble Dungeon/Assets/Scripts/BulletController.cs
@@ -0,0 +1,149 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Logic for handling events and movement for the bullet
+/// </summary>
+public class BulletController : MonoBehaviour {
+    #region Reference Variables
+
+    /// <summary>
+    /// Reference variable for our rigidbody
+    /// </summary>
+    private Rigidbody bulletRigidbody = null;
+
+    #endregion
+
+    #region Bullet Configs
+
+    /// <summary>
+    /// The speed of the bullet
+    /// </summary>
+    [SerializeField] private float speed = 5.0f;
+
+    /// <summary>
+    /// The X boundaries for the bullet
+    /// </summary>
+    [SerializeField] private float boundaryX = 50f;
+
+    /// <summary>
+    /// The Z boundaries for the bullet
+    /// </summary>
+    [SerializeField] private float boundaryZ = 50f;
+
+    /// <summary>
+    /// The amount of damage the bullet will inflict
+    /// </summary>
+    public int damage = 1;
+
+    /// <summary>
+    /// Indicates whether this bullet pierces through enemies
+    /// </summary>
+    public bool isPiercing = false;
+
+    #endregion
+
+    #region Calculation variables
+
+    /// <summary>
+    /// Calculation variable for the local velocity
+    /// </summary>
+    private Vector3 localVelocity = Vector3.zero;
+
+    /// <summary>
+    /// The position where bullet was spawned
+    /// </summary>
+    private Vector3 spawnPoint = Vector3.zero;
+
+    #endregion
+
+    #region Public Variables
+
+    /// <summary>
+    /// GameObject tag to damage
+    /// </summary>
+    public string damageTag = "";
+
+    /// <summary>
+    /// The tag of the object that spawn this bullet
+    /// </summary>
+    public string spawnTag = "";
+
+    #endregion
+
+    #region Helper Functions
+
+    /// <summary>
+    /// Check if bullet has passed its allowed boundaries
+    /// </summary>
+    /// <returns></returns>
+    private bool isPastBoundary()
+    {
+        if (Mathf.Abs(this.transform.position.x) > boundaryX || Mathf.Abs(this.transform.position.z) > boundaryZ)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    #endregion
+
+    #region Event Handlers
+
+    /// <summary>
+    /// Initialize variables
+    /// </summary>
+    private void Start()
+    {
+        bulletRigidbody = this.GetComponent<Rigidbody>();
+        spawnPoint = this.transform.position;
+    }
+
+	/// <summary>
+    /// Handle movement every frame and out of bounds calls
+    /// </summary>
+	private void Update()
+    {
+        // Handle movement
+        localVelocity = this.transform.TransformDirection(Vector3.forward * speed);
+        bulletRigidbody.velocity = localVelocity;
+
+        // Destroy when out of bounds
+        if (isPastBoundary())
+        {
+            GameObject.Destroy(this.gameObject);
+        }
+    }
+
+    /// <summary>
+    /// Handle collisions for the bullet
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnTriggerEnter(Collider other)
+    {
+        // Send "OnShot" message if the bullet collides with the correct object
+        string tag = other.gameObject.tag;
+        if (tag == damageTag)
+        {
+            other.gameObject.SendMessage("OnShot", damage, SendMessageOptions.DontRequireReceiver);
+        }
+
+        // Don't destroy bullet if it is a piercing type and it doesn't hit a wall
+        if (isPiercing && tag != "Wall")
+        {
+            return;
+        }
+
+        // Don't destroy bullet if the trigger is the one that spawned it
+        if (tag == spawnTag)
+        {
+            return;
+        }
+
+        // Otherwise, destroy the bullet
+        GameObject.Destroy(this.gameObject);
+    }
+
+    #endregion
+}
---
+++ GAM 215/Marble Dungeon/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,179 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Handles game logic for player events
+/// </summary>
+public class PlayerController : MonoBehaviour {
+    #region Reference Variables
+
+    /// <summary>
+    /// Our rigidbody
+    /// </summary>
+    private Rigidbody playerRigidbody = null;
+
+    /// <summary>
+    /// The game object to spawn when we fire a projectile
+    /// </summary>
+    [SerializeField] private GameObject projectile = null;
+
+    #endregion
+
+    #region Player Config
+
+    /// <summary>
+    /// Max health of the player
+    /// </summary>
+    [SerializeField] private int maxHealth = 3;
+
+    /// <summary>
+    /// The speed of the player
+    /// </summary>
+    [SerializeField] private float speed = 5.0f;
+
+    /// <summary>
+    /// The firing delay of the weapon in seconds
+    /// </summary>
+    [SerializeField] private float firingDelay = 0.25f;
+
+    /// <summary>
+    /// The amount of damage the player should do
+    /// </summary>
+    [SerializeField] private int damage = 1;
+
+    #endregion
+
+    #region Input Axis Config
+
+    /// <summary>
+    /// Input axis name for the horizontal movement
+    /// </summary>
+    [SerializeField] private string horizontalMovementInputName = "Horizontal";
+
+    /// <summary>
+    /// Input axis name for the vertical movement
+    /// </summary>
+    [SerializeField] private string verticalMovementInputName = "Vertical";
+
+    /// <summary>
+    /// Input axis name for firing a weapon
+    /// </summary>
+    [SerializeField] private string firingInputName = "Fire1";
+
+    #endregion
+
+    #region Calculation Variables
+
+    /// <summary>
+    /// The velocity we want to apply to our rigidbody
+    /// </summary>
+    private Vector3 velocity = Vector3.zero;
+
+    /// <summary>
+    /// Indicates the last time the player fired their weapon
+    /// </summary>
+    private float lastFired = 0;
+
+    /// <summary>
+    /// Ray we'll use when firing
+    /// </summary>
+    private Ray firingRay;
+
+    /// <summary>
+    /// The position we are firing towards
+    /// </summary>
+    private Vector3 targetPoint = Vector3.zero;
+
+    /// <summary>
+    /// The rotation of the projectile
+    /// </summary>
+    private Quaternion targetRotation = Quaternion.identity;
+
+    #endregion
+
+    #region Helper Functions
+
+    /// <summary>
+    /// The logic for moving the player
+    /// </summary>
+    /// <param name="horizontalValue">The amount we move left/right</param>
+    /// <param name="verticalValue">The amount we move up/down</param>
+    private void Move(float horizontalValue, float verticalValue)
+    {
+        // Apply the horizontal and vertical movement with the player's speed
+        velocity.x = horizontalValue * speed;
+        velocity.y = 0;
+        velocity.z = verticalValue * speed;
+
+        // Actually apply the velocity to the player's rigidbody
+        playerRigidbody.velocity = velocity;
+    }
+
+    /// <summary>
+    /// Logic for where to shoot the projectile and how
+    /// </summary>
+    private void FireWeapon()
+    {
+        // Generate a plane that intersects the transform's position with an upwards normal.
+        Plane playerPlane = new Plane(Vector3.up, transform.position);
+
+        // Generate a ray from the cursor position
+        firingRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        // Determine the point where the cursor ray intersects the plane.
+        // This will be the point that the object must look towards to be looking at the mouse.
+        // Raycasting to a Plane object only gives us a distance, so we'll have to take the distance,
+        //   then find the point along that ray that meets that distance.  This will be the point
+        //   to look at.
+        float hitdist = 0.0f;
+        // If the ray is parallel to the plane, Raycast will return false.
+        if (playerPlane.Raycast(firingRay, out hitdist))
+        {
+            // Get the point along the ray that hits the calculated distance.
+            targetPoint = firingRay.GetPoint(hitdist);
+
+            // Determine the target rotation.  This is the rotation if the transform looks at the target point.
+            targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+
+            // Create bullet and configure it
+            GameObject currentProjectile = Instantiate(projectile, this.transform.position, targetRotation) as GameObject;
+            BulletController bulletController = currentProjectile.GetComponent<BulletController>();
+            bulletController.damage = damage;
+            bulletController.damageTag = "Enemy";
+            bulletController.spawnTag = this.gameObject.tag;
+        }
+    }
+
+#endregion
+
+#region Event Handlers
+
+    /// <summary>
+    /// Initialize variables
+    /// </summary>
+    private void Start()
+    {
+        playerRigidbody = this.GetComponent<Rigidbody>();
+	}
+	
+	/// <summary>
+    /// Handle events every frame
+    /// </summary>
+	private void Update()
+    {
+        // Handle player movement
+        float horizontalValue = Input.GetAxis(horizontalMovementInputName);
+        float verticalValue = Input.GetAxis(verticalMovementInputName);
+        Move(horizontalValue, verticalValue);
+
+        // Trigger weapon fire if input is pressed and the weapon is not on cooldown
+        float now = Time.time;
+        if (Input.GetAxis(firingInputName) != 0 && now > lastFired + firingDelay)
+        {
+            lastFired = now;
+            FireWeapon();
+        }
+	}
+
+#endregion
+}

062b2256159abb35aec868ee0d0a299f8683f987
bluejv7 <bluejv7+github@gmail.com>
2016-08-02 00:55:02 -0700

    Updated animations for player; added camera controller

---
+++ GAM 215/Marble Dungeon/Assets/Scripts/CameraController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Handle events and updates for the camera
+/// </summary>
+public class CameraController : MonoBehaviour {
+    /// <summary>
+    /// Reference variable for player
+    /// </summary>
+    private GameObject player = null;
+
+    /// <summary>
+    /// Our initial position
+    /// </summary>
+    private Vector3 initialPosition = Vector3.zero;
+
+    /// <summary>
+    /// New position to update our transform with
+    /// </summary>
+    private Vector3 newPosition = Vector3.zero;
+
+	/// <summary>
+    /// Initialize variables
+    /// </summary>
+	private void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        initialPosition = this.transform.position;
+	}
+	
+	/// <summary>
+    /// Handle what happens each frame
+    /// </summary>
+	private void Update()
+    {
+        newPosition.x = player.transform.position.x;
+        newPosition.y = initialPosition.y;
+        newPosition.z = player.transform.position.z + initialPosition.z;
+        this.transform.position = newPosition;
+	}
+}
